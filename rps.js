684b486f3aa08e1309bb9fb4b0b106027157f14f
Arturo Cabrera <cabrera_a90@hotmail.com>
2026-01-09 12:31:59 -0600

    step2: pre-UI

--- rps.js
+++ rps.js
@@ -1,4 +1,12 @@
-var humanScore=0, computerScore=0;
+var humanScore=0, computerScore=0, roundsPlayed=0;
+
+var buttons=document.querySelectorAll("button");
+
+buttons.forEach(element=>{
+    element.addEventListener("click",playRound);
+});
+
+
 
 function getComputerChoice(){
     /*
@@ -7,15 +15,20 @@ function getComputerChoice(){
      * 2: scissor
      */
     var result= Math.trunc(Math.random()*3);
-    console.log("computer choice is"+result);
+    console.log("computer choice is "+result);
     return result;
 }
 
-function getHumanChoice(){
-    let choice=prompt("Your choice").toUpperCase().trim();
-    console.log(choice+": "+typeof(choice));
+function getHumanChoice(choice){
+    /*let choice=prompt("Your choice").toUpperCase().trim();
+    console.log(choice+": "+typeof(choice));*/
 
-    switch(choice){
+  /*  for(v in choice){
+        console.log(v,choice[v]);
+    }*/
+
+    console.log("human choice: "+choice);
+    switch(choice.toUpperCase().trim()){
         case "ROCK":
             choice=0;
             break;
@@ -27,19 +40,35 @@ function getHumanChoice(){
                 break;
         default:
                 choice=-1;
-                alert("invalid option");
+                //alert("invalid option");
                 break;
     }
     return choice;
 }
 
 
-function playRound(humanChoice, computerChoice){
+function playRound(event){
+    const boton=event.target;
+    //console.log("hiciste click en ", boton);
+
+    let value="";
+    //console.log(boton.value);
+    for(const v in boton){
+        if(v.toString()=="firstChild")
+            value=boton[v];
+    }
+    console.log("linea 60, typeof value:",typeof(value),"valor",value.wholeText);
+    
+    //choice, computerChoice
+    var humanChoice=getHumanChoice(value.wholeText), computerChoice=getComputerChoice();
+    
+
     if(humanChoice<0 || humanChoice>2){
         return -1;
     }
     var result=humanChoice-computerChoice;
     if(result == 0){
+        
         console.log("DRAW");
         return 0;
     }
@@ -50,14 +79,15 @@ function playRound(humanChoice, computerChoice){
     }
     console.log("You lose!")
     computerScore++;
+    roundsPlayed++;
     return 0;
 }
 
 function playGame(){
-    for(var i=0; i<5; i++){
+    /*for(var i=0; i<5; i++){
         i+=playRound(getHumanChoice(),getComputerChoice());
-    }
+    }*/
 }
 
 playGame();
-console.log("Final score\nHuman: "+humanScore+"Computer: "+computerScore);
+console.log("Final score\nHuman: "+humanScore+"\nComputer: "+computerScore);
